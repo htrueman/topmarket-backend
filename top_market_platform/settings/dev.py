341e4325b569452c7ea103277bbafa6a16bae943
Khomenko §mykhailo <mykhailo@I-Macs-iMac-2.local>
2019-03-15 15:43:54 +0200

    Added email (console) backend

--- top_market_platform/settings/dev.py
+++ top_market_platform/settings/dev.py
@@ -2,3 +2,12 @@ from .base import *
 
 
 DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = "<rz`JG_st'y23j[L"
+EMAIL_HOST_USER = 'market.service@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
