cd8749eb9e0d2da625b70d0439dc3b9532c13cd8
Khomenko §mykhailo <mykhailo@I-Macs-iMac-2.local>
2019-04-12 15:05:26 +0300

    Added email smtp

--- top_market_platform/settings/production.py
+++ top_market_platform/settings/production.py
@@ -1,3 +1,12 @@
 from .base import *
 
 DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = 'no-reply@topmarket.club'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
