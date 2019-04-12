2d7b80f1d732e75a85d8ba2af95b0fe0a1dac84e
Mike Storcheous <dolbager@gmail.com>
2019-04-12 17:52:07 +0300

    FIX Dockerfile last =)

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "====== RUN MIGRATIONS ======"
 python3 manage.py migrate
 
 echo "====== COLLECT STATIC ======"
-python3 manage.py collectstatic
+python3 manage.py collectstatic -c --noinput
 
 echo "====== RUN APP ======"
 gunicorn -b 0.0.0.0:8080 -w 4 top_market_platform.wsgi
