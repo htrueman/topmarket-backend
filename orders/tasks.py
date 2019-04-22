3119a2028f5ebe240d36e3cc1793cfe561835f6f
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-22 17:00:58 +0300

    migrations fixed

--- orders/tasks.py
+++ orders/tasks.py
@@ -19,8 +19,8 @@ def checkout_orders():
     for user in User.objects.filter(role='PARTNER'):
         if (Company.objects.filter(user=user).exists()
                 and MyStore.objects.filter(user=user).exists()):
-            token = cache.get('user_id_{}'.format(user.pk))
-            if not token:
+            token_rozetka = cache.get('user_id_{}'.format(user.pk))
+            if not token_rozetka:
                 if user.rozetka_username and user.rozetka_password:
                     curl_get_access_key = 'curl -X POST https://api.seller.rozetka.com.ua/sites ' \
                                    '-H \'Content-Type: application/json\' ' \
@@ -33,14 +33,14 @@ def checkout_orders():
                     data = json.loads(output)
 
                     if data['success']:
-                        token = data['content']['access_token']
-                        cache.set('user_id_{}'.format(user.pk), token, 60 * 60 * 24)
-            if token:
+                        token_rozetka = data['content']['acces_rozetkas_token']
+                        cache.set('user_id_{}'.format(user.pk), token_rozetka, 60 * 60 * 24)
+            if token_rozetka:
                 curl_get_orders_key = 'curl -X GET https://api.seller.rozetka.com.ua/orders/search' \
                                       '?expand=user,delivery,order_status_history ' \
-                                      '-H \'Authorization: Bearer {token}\' ' \
+                                      '-H \'Authorization: Bearer {token_rozetka}\' ' \
                                       '-H \'cache-control: no-cache\'' \
-                    .format(token=token)
+                    .format(token_rozetka=token_rozetka)
                 output = subprocess.check_output(curl_get_orders_key, stderr=PIPE, shell=True)
                 data = json.loads(output)
 
