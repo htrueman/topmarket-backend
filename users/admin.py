78754e53140689caec921c9f8d5a806a7654c5fe
Khomenko §mykhailo <mykhailo@I-Macs-iMac-2.local>
2019-04-22 14:31:52 +0300

    Added company's params to admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser, Company, MyStore, CompanyPitch, Passport
+from .models import CustomUser, Company, MyStore, CompanyPitch, Passport, ActivityAreas, ServiceIndustry, CompanyType
 
 admin.site.register(CustomUser)
 admin.site.register(Company)
 admin.site.register(MyStore)
 admin.site.register(CompanyPitch)
 admin.site.register(Passport)
+admin.site.register(ServiceIndustry)
+admin.site.register(ActivityAreas)
+admin.site.register(CompanyType)
