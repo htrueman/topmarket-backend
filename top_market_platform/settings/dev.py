c5657ed2835b59fa04b31b0a6744eb2672f7ce52
Khomenko §mykhailo <mykhailo@I-Macs-iMac-2.local>
2019-04-23 11:25:10 +0300

    Added email sendgrid

--- top_market_platform/settings/dev.py
+++ top_market_platform/settings/dev.py
@@ -5,10 +5,10 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = "<rz`JG_st'y23j[L"
-EMAIL_HOST_USER = 'market.service@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_PASSWORD = "1q2w3e4r5t6y"
+EMAIL_HOST_USER = 'Nazar3000'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = 'no-reply@topmarket.club'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
