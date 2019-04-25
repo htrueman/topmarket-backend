a4d86b74d1f66decd3f87c45caf351e5af6f8088
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-25 21:13:48 +0300

    dummy

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -67,58 +67,57 @@ def load_products_from_xls(**kwargs):
             raise ValidationError(_('Invalid headers in file'))
 
         token_rozetka = get_rozetka_auth_token(prod_hist.user)
-        with transaction.atomic():
-            if token_rozetka:
-                for product_id in result_tuple:
-                    # curl_get_orders_key = 'curl -X GET https://api.seller.rozetka.com.ua/items/{product_id}' \
-                    #                       '?expand=sell_status,sold,status,description,description_ua,' \
-                    #                       'details,parent_category,status_available,group_item ' \
-                    #                       '-H \'Authorization: Bearer {token_rozetka}\' ' \
-                    #                       '-H \'cache-control: no-cache\'' \
-                    #     .format(token_rozetka=token_rozetka, product_id=product_id)
-                    # output = subprocess.check_output(curl_get_orders_key, stderr=subprocess.PIPE, shell=True)
-                    # data = json.loads(output)
+        # with transaction.atomic():
+        if token_rozetka:
+            for product_id in result_tuple:
+                # curl_get_orders_key = 'curl -X GET https://api.seller.rozetka.com.ua/items/{product_id}' \
+                #                       '?expand=sell_status,sold,status,description,description_ua,' \
+                #                       'details,parent_category,status_available,group_item ' \
+                #                       '-H \'Authorization: Bearer {token_rozetka}\' ' \
+                #                       '-H \'cache-control: no-cache\'' \
+                #     .format(token_rozetka=token_rozetka, product_id=product_id)
+                # output = subprocess.check_output(curl_get_orders_key, stderr=subprocess.PIPE, shell=True)
+                # data = json.loads(output)
 
-                    url = "https://api.seller.rozetka.com.ua/items/{product_id}" \
-                          "?expand=sell_status,sold,status,description,description_ua" \
-                          ",details,parent_category,status_available,group_item"\
-                        .format(product_id=product_id)
-                    headers = {
-                        'Authorization': "Bearer {}".format(token_rozetka),
-                        'cache-control': "no-cache"
-                    }
-                    r = requests.Request("GET", url, headers=headers)
-                    prep = r.prepare()
-                    s = requests.Session()
-                    resp = s.send(prep)
-                    r.encoding = 'utf-8'
-                    data = resp.json()
+                url = "https://api.seller.rozetka.com.ua/items/{product_id}" \
+                      "?expand=sell_status,sold,status,description,description_ua" \
+                      ",details,parent_category,status_available,group_item"\
+                    .format(product_id=product_id)
+                headers = {
+                    'Authorization': "Bearer {}".format(token_rozetka),
+                    'cache-control': "no-cache"
+                }
+                r = requests.Request("GET", url, headers=headers)
+                prep = r.prepare()
+                s = requests.Session()
+                resp = s.send(prep)
+                r.encoding = 'utf-8'
+                data = resp.json()
 
-                    if data['success']:
-                        product = data['content']
-                        # Массажер для чистки лица + POBLING + Sonic Pore Cleansing Brush + Golden
-                        full_name = product.get('name') if product.get('name') else product.get('name_ua')
+                if data['success']:
+                    product = data['content']
+                    # Массажер для чистки лица + POBLING + Sonic Pore Cleansing Brush + Golden
+                    full_name = product.get('name') if product.get('name') else product.get('name_ua')
 
-                        with suppress(Exception):
-                            product_instance, created = Product.objects.update_or_create(
-                                rozetka_id=product['id'],
-                                user=prod_hist.user,
-                                defaults={
-                                    'name': product.get('name') if product.get('name') else product.get('name_ua'),
-                                    'vendor_code': product['article'],
-                                    'price': product['price'],
-                                    'category_id': product['catalog_id'],
-                                    'description': product.get('description')
-                                    if product.get('description') else product.get('description_ua'),
-                                }
+                    with suppress(Exception):
+                        product_instance, created = Product.objects.update_or_create(
+                            rozetka_id=product['id'],
+                            user=prod_hist.user,
+                            defaults={
+                                'name': product.get('name') if product.get('name') else product.get('name_ua'),
+                                'vendor_code': product['article'],
+                                'price': product['price'],
+                                'category_id': product['catalog_id'],
+                                'description': product.get('description')
+                                if product.get('description') else product.get('description_ua'),
+                            }
+                        )
+
+                        for photo in product['photo']:
+                            ProductImageURL.objects.update_or_create(
+                                product=product_instance,
+                                url=photo
                             )
-
-                            for photo in product['photo']:
-                                ProductImageURL.objects.update_or_create(
-                                    product=product_instance,
-                                    url=photo
-                                )
-                    time.sleep(0.7)
-                prod_hist.is_uploaded = True
-                prod_hist.save()
-        return token_rozetka
+                # time.sleep(0.7)
+            prod_hist.is_uploaded = True
+            prod_hist.save()
