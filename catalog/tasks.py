575cf9111f14830926eafb38bfd0bd00dd89b114
Khomenko §mykhailo <mykhailo@I-Macs-iMac-2.local>
2019-04-26 16:21:21 +0300

    Added exception to invalid xls file

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -1,10 +1,8 @@
-import json
-import subprocess
 from contextlib import suppress
 
 import requests
+from django.core.cache import cache
 from django.db import transaction
-from django.db.utils import IntegrityError
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from import_export.results import RowResult
@@ -12,12 +10,14 @@ from import_export.results import RowResult
 from catalog.constants import ProductUploadFileTypes
 from catalog.resources import ProductResource
 from tablib import Dataset
-from .models import ProductUploadHistory, Product, ProductImageURL
+
+from catalog.serializers import CategorySerializer
+from top_market_platform.celery import app
+from .models import ProductUploadHistory, Product, ProductImageURL, Category
 from django.utils.translation import ugettext as _
 from rest_framework.exceptions import ValidationError
 from catalog.utils import get_rozetka_auth_token
 import xlrd
-import time
 
 logger = get_task_logger(__name__)
 
@@ -60,71 +60,89 @@ def load_products_from_xls(**kwargs):
                 prod_hist.errors = error
                 prod_hist.save()
     elif prod_hist.file_type == ProductUploadFileTypes.ROZETKA:
-        workbook = xlrd.open_workbook(file_path)
-        sheet = workbook.sheet_by_index(0)
-        if sheet.cell_value(rowx=0, colx=0) == 'ID товара в розетке':
-            product_id_list = sheet.col_slice(0, start_rowx=1)
-            list_with_zero = map(lambda x: int(x.value), product_id_list)
-            result_tuple = tuple(filter(lambda x: x if x > 0 else None, list_with_zero))
-        else:
-            raise ValidationError(_('Invalid headers in file'))
+        try:
+            workbook = xlrd.open_workbook(file_path)
+            sheet = workbook.sheet_by_index(0)
+            if sheet.cell_value(rowx=0, colx=0) == 'ID товара в розетке':
+                product_id_list = sheet.col_slice(0, start_rowx=1)
+                list_with_zero = map(lambda x: int(x.value), product_id_list)
+                result_tuple = tuple(filter(lambda x: x if x > 0 else None, list_with_zero))
+            else:
+                raise ValidationError(_('Invalid headers in file'))
 
-        token_rozetka = get_rozetka_auth_token(prod_hist.user)
-        # with transaction.atomic():
-        if token_rozetka:
-            count = 0
-            for product_id in result_tuple:
-                # curl_get_orders_key = 'curl -X GET https://api.seller.rozetka.com.ua/items/{product_id}' \
-                #                       '?expand=sell_status,sold,status,description,description_ua,' \
-                #                       'details,parent_category,status_available,group_item ' \
-                #                       '-H \'Authorization: Bearer {token_rozetka}\' ' \
-                #                       '-H \'cache-control: no-cache\'' \
-                #     .format(token_rozetka=token_rozetka, product_id=product_id)
-                # output = subprocess.check_output(curl_get_orders_key, stderr=subprocess.PIPE, shell=True)
-                # data = json.loads(output)
+            token_rozetka = get_rozetka_auth_token(prod_hist.user)
+            # with transaction.atomic():
+            if token_rozetka:
+                count = 0
+                for product_id in result_tuple:
+                    # curl_get_orders_key = 'curl -X GET https://api.seller.rozetka.com.ua/items/{product_id}' \
+                    #                       '?expand=sell_status,sold,status,description,description_ua,' \
+                    #                       'details,parent_category,status_available,group_item ' \
+                    #                       '-H \'Authorization: Bearer {token_rozetka}\' ' \
+                    #                       '-H \'cache-control: no-cache\'' \
+                    #     .format(token_rozetka=token_rozetka, product_id=product_id)
+                    # output = subprocess.check_output(curl_get_orders_key, stderr=subprocess.PIPE, shell=True)
+                    # data = json.loads(output)
 
-                url = "https://api.seller.rozetka.com.ua/items/{product_id}" \
-                      "?expand=sell_status,sold,status,description,description_ua" \
-                      ",details,parent_category,status_available,group_item"\
-                    .format(product_id=product_id)
-                headers = {
-                    'Authorization': "Bearer {}".format(token_rozetka),
-                    'cache-control': "no-cache"
-                }
-                r = requests.Request("GET", url, headers=headers)
-                prep = r.prepare()
-                s = requests.Session()
-                resp = s.send(prep)
-                r.encoding = 'utf-8'
-                data = resp.json()
-                if data['success']:
-                    product = data['content']
-                    # Массажер для чистки лица + POBLING + Sonic Pore Cleansing Brush + Golden
-                    full_name = product.get('name') if product.get('name') else product.get('name_ua')
+                    url = "https://api.seller.rozetka.com.ua/items/{product_id}" \
+                          "?expand=sell_status,sold,status,description,description_ua" \
+                          ",details,parent_category,status_available,group_item" \
+                        .format(product_id=product_id)
+                    headers = {
+                        'Authorization': "Bearer {}".format(token_rozetka),
+                        'cache-control': "no-cache"
+                    }
+                    r = requests.Request("GET", url, headers=headers)
+                    prep = r.prepare()
+                    s = requests.Session()
+                    resp = s.send(prep)
+                    r.encoding = 'utf-8'
+                    data = resp.json()
+                    if data['success']:
+                        product = data['content']
+                        # Массажер для чистки лица + POBLING + Sonic Pore Cleansing Brush + Golden
+                        full_name = product.get('name') if product.get('name') else product.get('name_ua')
 
-                    with suppress(Exception):
-                        product_instance, created = Product.objects.update_or_create(
-                            rozetka_id=product['id'],
-                            user=prod_hist.user,
-                            defaults={
-                                'name': product.get('name') if product.get('name') else product.get('name_ua'),
-                                'vendor_code': product['article'],
-                                'price': product['price'],
-                                'category_id': product['catalog_id'],
-                                'description': product.get('description')
-                                if product.get('description') else product.get('description_ua'),
-                            }
-                        )
-                        if created:
-                            count += 1
-
-                        for photo in product['photo']:
-                            ProductImageURL.objects.update_or_create(
-                                product=product_instance,
-                                url=photo
+                        with suppress(Exception):
+                            product_instance, created = Product.objects.update_or_create(
+                                rozetka_id=product['id'],
+                                user=prod_hist.user,
+                                defaults={
+                                    'name': product.get('name') if product.get('name') else product.get('name_ua'),
+                                    'vendor_code': product['article'],
+                                    'price': product['price'],
+                                    'category_id': product['catalog_id'],
+                                    'description': product.get('description')
+                                    if product.get('description') else product.get('description_ua'),
+                                }
                             )
-                # time.sleep(0.7)
-            prod_hist.total_products_count = len(result_tuple)
-            prod_hist.imported_products_count = count
+                            if created:
+                                count += 1
+
+                            for photo in product['photo']:
+                                ProductImageURL.objects.update_or_create(
+                                    product=product_instance,
+                                    url=photo
+                                )
+                    # time.sleep(0.7)
+                prod_hist.total_products_count = len(result_tuple)
+                prod_hist.imported_products_count = count
+                prod_hist.is_uploaded = True
+                prod_hist.save()
+
+        except Exception:
+            prod_hist.errors = 'Не правильный формат файла. После импорта с розетки, пересохраните файл!'
             prod_hist.is_uploaded = True
             prod_hist.save()
+
+
+
+
+# @app.task
+# def load_categories():
+#     with transaction.atomic():
+#         Category.load_categories()
+#         queryset = Category.objects.root_nodes()
+#         serializer = CategorySerializer(queryset)
+#         cache.set('categories', serializer.data)
+
