751ae0c01194b575fd81ddda57919f3c84a32c9b
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-06 13:23:23 +0300

    load_categories fixef

--- top_market_platform/celery.py
+++ top_market_platform/celery.py
@@ -16,6 +16,6 @@ app.conf.CELERYBEAT_SCHEDULE = {
     },
     "load_categories": {
         "task": "catalog.tasks.load_categories",
-        "schedule": crontab(minute="*/35")
+        "schedule": crontab(minute="*/10")
     }
 }
